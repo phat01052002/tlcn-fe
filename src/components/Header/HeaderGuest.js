88e9823bb920be5fada2b9e8a32e0892f8f9130b
unknown <chauthuanphat10@gmail.com>
2023-09-21 17:44:41 +0700

    21/9

--- src/components/Header/HeaderGuest.js
+++ src/components/Header/HeaderGuest.js
@@ -32,7 +32,7 @@ export default function HeaderGuest({guestOrUser}) {
             <div className='header-guest row'>
                 <div className='col-1'>
                 </div>
-                <div className='col-1'>
+                <div className='col-1 linkToGuest'>
                 <a href='/guest'>
                         <img src={logo} className='logo-img'></img>
                     </a>
@@ -88,16 +88,16 @@ export default function HeaderGuest({guestOrUser}) {
             <div className='header-guest-new'>
             <div className='row'>
                 <div className='col-1'>
+                </div>
+                <div className='col-1 linkToGuest' >
                     <a href='/guest'>
                         <img src={logo} className='logo-img'></img>
                     </a>
                 </div>
-                <div className='col-1'>
-                </div>
                 <div className='col-5 search'>
                     <span className='icon-search'>
                         <button className='btn btn-search'>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
                             <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
                         </svg>
                         </button>
@@ -107,7 +107,7 @@ export default function HeaderGuest({guestOrUser}) {
                 </div>
                 <div className='col-1'></div>
                 <div className='login-btn col-2'>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
                         <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
                     </svg>
                     &nbsp;
@@ -115,7 +115,7 @@ export default function HeaderGuest({guestOrUser}) {
                 </div>
                 <div className='cart-btn col-1'>
                     <a className='cart' href='/cart'>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-cart3" viewBox="0 0 16 16">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-cart3" viewBox="0 0 16 16">
                             <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
                         </svg>
                         Giỏ hàng
