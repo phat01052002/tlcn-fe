e4486d6d30ebe052c51a01a2744d4b4f13b0eeef
unknown <chauthuanphat10@gmail.com>
2023-10-05 14:57:24 +0700

    10/5

--- src/components/Category/PageCategory.js
+++ src/components/Category/PageCategory.js
@@ -1,11 +1,39 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 import {useParams} from 'react-router-dom'
 import HeaderGuest from '../Header/HeaderGuest'
+import ListProductByCategory from './ListProductByCategory'
 export default function PageCategory() {
     const {categoryId}=useParams()
+    const [category,setCategory]=useState([])
+    const [listProduct,setListProduct]=useState([])
+    useEffect(()=>{
+         axios.get(`/getCategory/${categoryId}`)
+        .then(res => setCategory(res.data))
+        .catch(err => console.log(err))
+    },[])
+
+    //get product by Category
+
+    useEffect(()=>{
+        axios.get(`/productsByCategory/${categoryId}`)
+        .then(res => setListProduct(res.data))
+        .catch(err => console.log(err))
+    },[])
     return (
         <div>
            <HeaderGuest guestOrUser={false}/>
+           <div className='page-category'>
+                <div className='page-category-img'>
+                    <h3>{category.name}</h3>
+                    <span>
+                        <a href='/guest'>Trang chủ</a>
+                    </span>
+                </div>
+           </div>
+           <div className='product-by-category'>
+                <ListProductByCategory listProduct={listProduct}/>
+           </div>
            <br/>
       <br/>
       <br/>
