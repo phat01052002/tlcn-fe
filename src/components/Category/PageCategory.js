90c4c33a8511a2fb07ba16c96485381a7d30522b
unknown <chauthuanphat10@gmail.com>
2023-10-06 22:07:54 +0700

    mess

--- src/components/Category/PageCategory.js
+++ src/components/Category/PageCategory.js
@@ -20,8 +20,29 @@ export default function PageCategory() {
         .then(res => setListProduct(res.data))
         .catch(err => console.log(err))
     },[])
+    ////////////////////////
+    var chatbox = document.getElementById('fb-customer-chat');
+    chatbox.setAttribute("page_id", "106002218749895");
+    chatbox.setAttribute("attribution", "biz_inbox");
+    window.fbAsyncInit = function() {
+        FB.init({
+          xfbml            : true,
+          version          : 'v18.0'
+        });
+      };
+
+      (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
     return (
         <div>
+    <div id="fb-root"></div>
+    <div id="fb-customer-chat" class="fb-customerchat">
+    </div>
            <HeaderGuest/>
            <div className='page-category'>
                 <div className='page-category-img'>
