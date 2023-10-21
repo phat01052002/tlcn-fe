adbae07fd488cedf6a142730b012f72ef9b92e6e
unknown <chauthuanphat10@gmail.com>
2023-10-22 03:30:53 +0700

    login design

--- src/Login/PageLogin.js
+++ src/Login/PageLogin.js
@@ -30,65 +30,59 @@ export default function PageLogin() {
             //save access token to sessionStorage
             sessionStorage.setItem('USER', JSON.stringify(response.data));
             //reload
-            dispatch(changeRole('user'))
-            nav('/')
+            dispatch(changeRole('user'));
+            nav('/');
         } catch {
             alert('Tài khoản mật khẩu không đúng');
         }
     }, []);
     return (
-        <div className="page-login row">
-            <div className="col-4"></div>
-            <div className="col-4">
-                <div className="form-control form-login ">
-                    <br />
-                    <h5>ĐĂNG NHẬP</h5>
-                    <br />
-                    <input id="name" className="form-control input-name" placeholder="Tên đăng nhập"></input>
-                    <br />
-                    <input
-                        id="password"
-                        type="password"
-                        className="form-control input-password"
-                        placeholder="Mật khẩu"
-                    ></input>
-                    <br />
-                    <button className="form-control btn btn-primary btn-login" onClick={handleClickLogin}>
-                        Đăng Nhập
-                    </button>
-                    <br />
-                    <br />
-                    <button className="form-control btn btn-danger btn-login">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="16"
-                            height="14"
-                            fill="currentColor"
-                            class="bi bi-google"
-                            viewBox="0 0 16 16"
-                        >
-                            <path d="M15.545 6.558a9.42 9.42 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.689 7.689 0 0 1 5.352 2.082l-2.284 2.284A4.347 4.347 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.792 4.792 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.702 3.702 0 0 0 1.599-2.431H8v-3.08h7.545z" />
-                        </svg>
-                        &nbsp; Đăng Nhập Bằng Gmail
-                    </button>
-                    <br />
-                    <br />
-                    <div className=" row">
-                        <div className="col-4">
-                            <a className="forgot-password-link" href="/">
-                                Quên mật khẩu
-                            </a>
+        <div className="page-login">
+            <div className="row">
+                <div className="col-2"></div>
+                <div className="col-8 ">
+                    <div className="form-login">
+                        <div className="title-login">
+                            ĐĂNG NHẬP
                         </div>
-                        <div className="col-5"></div>
-                        <div className="col-3">
-                            <a className="register-link" href="/register">
-                                Đăng ký
-                            </a>
+                        <div className="input-name">
+                            <input id="name" placeholder="Tên đăng nhập"></input>
+                        </div>
+                        <div className="input-password">
+                            <input
+                                id="password"
+                                type="password"
+                                className=" input-password"
+                                placeholder="Mật khẩu"
+                            ></input>
+                        </div>
+                        <div className="btn-login">
+                            <button onClick={handleClickLogin}>Đăng Nhập</button>
+                        </div>
+                        <div className="forgot-password-link login-link">
+                            <a href="/">Quên mật khẩu</a>
+                        </div>
+                        <div className="btn-login-gmail">
+                            <button className="btn-login">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="16"
+                                    height="14"
+                                    fill="currentColor"
+                                    class="bi bi-google"
+                                    viewBox="0 0 16 16"
+                                >
+                                    <path d="M15.545 6.558a9.42 9.42 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.689 7.689 0 0 1 5.352 2.082l-2.284 2.284A4.347 4.347 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.792 4.792 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.702 3.702 0 0 0 1.599-2.431H8v-3.08h7.545z" />
+                                </svg>
+                            </button>
+                        </div>
+                        <div className="register-link login-link">
+                            <a href="/register">Đăng ký</a>
                         </div>
                     </div>
                 </div>
+                <div className="col-2"></div>
             </div>
-            <div className="col-4"></div>
         </div>
     );
 }
